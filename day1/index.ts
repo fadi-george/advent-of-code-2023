d8d92ed4db0eb197c23678d3f205607fb4e96c32
Fadi George <fadii925@gmail.com>
2023-12-02 22:30:21 -0800

    d1 p2

--- day1/index.ts
+++ day1/index.ts
@@ -1,31 +1,22 @@
 import { readInput } from "../helpers";
 
 const dir = import.meta.dir;
-const data = readInput(dir, { sampleName: "sample", useSample: false });
+const data = readInput(dir);
 
 const group = `one|two|three|four|five|six|seven|eight|nine|zero|\\d`;
 const regex = new RegExp(`(${group})(?:.*(${group}))?`);
 
-// const p1 = data
-//   .map((line) => {
-//     let [_, a, b] = line.match(/(\d)(?:.*(\d))?/) ?? [];
-//     if (!b) b = a;
-//     return Number(a + b);
-//   })
-//   .reduce((a, b) => a + b, 0);
-// console.log(p1);
-
 const numberMap = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
-  zero: 0,
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+  zero: "0",
 };
 
 const res = data
@@ -33,8 +24,8 @@ const res = data
     let [_, a, b] = line.match(regex) ?? [];
     if (!b) b = a;
 
-    if (a in numberMap) a = numberMap[a];
-    if (b in numberMap) b = numberMap[b];
+    if (a in numberMap) a = numberMap[a as keyof typeof numberMap];
+    if (b in numberMap) b = numberMap[b as keyof typeof numberMap];
 
     return Number(a + b);
   })
--- helpers.ts
+++ helpers.ts
@@ -1,15 +1,9 @@
 import fs from "fs";
 import path from "path";
 
-export const readInput = (
-  dir: string,
-  {
-    useSample = false,
-    sampleName = "sample",
-    regex = "\n",
-  }: { useSample?: boolean; sampleName?: string; regex?: string } = {}
-) => {
-  const textPath = useSample ? `${sampleName}.txt` : "input.txt";
-  return fs.readFileSync(path.join(dir, textPath), "utf8").split(regex);
+export const readInput = (dir: string, regex = "\n") => {
+  return fs
+    .readFileSync(path.join(dir, `${import.meta.input || "input"}.txt`), "utf8")
+    .split(regex);
   return [];
 };
