18b6e1aa34ba9dfc56c92d28db67b6f4916e17eb
Fadi George <fadii925@gmail.com>
2023-12-08 22:02:06 -0800

    clean up

--- day9/index.ts
+++ day9/index.ts
@@ -1,39 +1,35 @@
-import { lcm, readInput } from "../helpers";
+import { readInput } from "../helpers";
 
 const dir = import.meta.dir;
 const lines = readInput(dir);
 
 // part 1
-const nextNums = lines.map((line) => {
+const tris = lines.map((line) => {
   const vals = line.match(/(-?\d)+/g)!.map(Number);
 
-  let triArr = [vals];
+  let tri = [vals];
   let i = 0;
 
-  while (!triArr[i].every((n) => n === 0)) {
-    let nextArr = [];
-    for (let j = 1; j < triArr[i].length; j++) {
-      nextArr.push(triArr[i][j] - triArr[i][j - 1]);
-    }
+  while (tri[i].some((n) => n !== 0)) {
+    let next = [];
+    for (let j = 1; j < tri[i].length; j++)
+      next.push(tri[i][j] - tri[i][j - 1]);
 
-    triArr.push(nextArr);
+    tri.push(next);
     i++;
   }
-  return triArr;
+  return tri;
 });
-const p1 = nextNums.reduce(
+const p1 = tris.reduce(
   (acc, arrs) => acc + arrs.reduce((acc, arr) => acc + arr[arr.length - 1], 0),
   0
 );
 console.log("Part 1: ", p1);
 
 // part 2
-const p2 = nextNums.reduce((acc, arrs) => {
-  let prev = 0;
-  for (let i = arrs.length - 2; i >= 0; i--) {
-    prev = arrs[i][0] - prev;
-  }
-
-  return acc + prev;
-}, 0);
+const p2 = tris.reduce(
+  (acc, arrs) =>
+    acc + arrs.slice(0, arrs.length - 1).reduceRight((a, r) => r[0] - a, 0),
+  0
+);
 console.log("Part 2: ", p2);
