df0f3382017afe119ad7f469504c16da4a1c3ec9
Fadi George <fadii925@gmail.com>
2023-12-10 22:05:37 -0800

    day 11 clean up

--- day11/index.ts
+++ day11/index.ts
@@ -1,6 +1,7 @@
 import { readInput } from "../helpers";
 
-const inds: Record<string, [number, number]> = {};
+type GMap = Record<string, [number, number]>;
+const gMap: GMap = {};
 const dir = import.meta.dir;
 const lines = readInput(dir);
 
@@ -11,96 +12,67 @@ let emptyCols: number[] = [];
 
 lines.forEach((line, rI) => {
   const matches = [...line.matchAll(/#/g)];
-  // if (!matches.length) emptyRows.push(rI);
-  if (matches) {
-    matches.forEach((g) => {
-      inds[`${i++}`] = [rI, g.index!];
-    });
-  }
+  if (!matches.length) emptyRows.push(rI); // no galaxies in this row
+  if (matches) matches.forEach((g) => (gMap[`${i++}`] = [rI, g.index!]));
 });
 
 // find empty columns
-let tmp = Object.values(inds).sort((a, b) => a[1] - b[1]);
+// distance must ge greater than 1 to be empty
+let tmp = Object.values(gMap).sort((a, b) => a[1] - b[1]);
 for (let i = 0; i < tmp.length - 1; i++) {
-  let [r, c] = tmp[i];
-  let [r2, c2] = tmp[i + 1];
-  if (c2 - c > 1) {
-    for (let j = c + 1; j < c2; j++) {
-      emptyCols.push(j);
-    }
-  }
-}
-
-// find empty rows
-tmp = Object.values(inds).sort((a, b) => a[0] - b[0]);
-for (let i = 0; i < tmp.length - 1; i++) {
-  let [r, c] = tmp[i];
-  let [r2, c2] = tmp[i + 1];
-  if (r2 - r > 1) {
-    for (let j = r + 1; j < r2; j++) {
-      emptyRows.push(j);
-    }
-  }
+  let [, c] = tmp[i];
+  let [, c2] = tmp[i + 1];
+  if (c2 - c > 1) for (let j = c + 1; j < c2; j++) emptyCols.push(j);
 }
 
 // find pairs of distances
 const getDistances = (scale = 1) => {
-  const newInds = structuredClone(inds);
+  const newMap = structuredClone(gMap);
 
   // adjust indices
-  const offsets = Object.keys(newInds).reduce<Record<string, [number, number]>>(
-    (acc, g) => {
-      acc[g] = [0, 0];
-      return acc;
-    },
+  const offsets = Object.keys(newMap).reduce<GMap>(
+    (acc, g) => (acc[g] = [0, 0]) && acc,
     {}
   );
 
-  const gKeys = Object.keys(newInds);
+  const gKeys = Object.keys(newMap);
   emptyCols.forEach((col) => {
     gKeys.forEach((g) => {
-      const [r, c] = newInds[g];
-      if (c > col) {
-        offsets[g][1] += scale - 1; // expand right
-      }
+      const [r, c] = newMap[g];
+      if (c > col) offsets[g][1] += scale - 1; // expand right
     });
   });
   emptyRows.forEach((row) => {
     gKeys.forEach((g) => {
-      const [r, c] = newInds[g];
-      if (r > row) {
-        offsets[g][0] += scale - 1; // expand down
-      }
+      const [r, c] = newMap[g];
+      if (r > row) offsets[g][0] += scale - 1; // expand down
     });
   });
 
   // adjust for expansion
-  Object.keys(newInds).forEach((g) => {
-    const [r, c] = newInds[g];
-    newInds[g] = [r + offsets[g][0], c + offsets[g][1]];
+  Object.keys(newMap).forEach((g) => {
+    const [r, c] = newMap[g];
+    newMap[g] = [r + offsets[g][0], c + offsets[g][1]];
   });
 
   const distances: number[] = [];
   const pairSet = new Set<string>();
-  Object.keys(newInds).forEach((g) => {
-    Object.keys(newInds).forEach((g2) => {
+  Object.keys(newMap).forEach((g) => {
+    Object.keys(newMap).forEach((g2) => {
       if (g === g2) return;
       let key = g < g2 ? `${g},${g2}` : `${g2},${g}`;
 
       if (!pairSet.has(key)) {
-        const [r, c] = newInds[g];
-        const [r2, c2] = newInds[g2];
+        const [r, c] = newMap[g];
+        const [r2, c2] = newMap[g2];
 
         distances.push(Math.abs(r - r2) + Math.abs(c - c2));
         pairSet.add(key);
       }
     });
   });
-
-  return distances;
+  return distances.sum();
 };
 
-console.log("Part 1: ", getDistances(2).sum());
-
-// 15208074 wrong
-console.log("Part 2: ", getDistances(1000000).sum());
+console.log("Part 1: ", getDistances(2));
+console.log("Part 2: ", getDistances(1000000));
