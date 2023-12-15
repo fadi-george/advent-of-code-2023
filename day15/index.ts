47dc2678655f7d9ad175a8283648c4d867a996e5
Fadi George <fadii925@gmail.com>
2023-12-14 21:49:09 -0800

    day 15 p2

--- day15/index.ts
+++ day15/index.ts
@@ -1,18 +1,54 @@
-import { printGrid, readInput, rotateRight } from "../helpers";
+import { readInput } from "../helpers";
 
 const dir = import.meta.dir;
-const lines = readInput(dir, ",");
-
-const hashes = lines.map((str) => {
-  let v = 0;
-  [...str].forEach((c) => {
-    v += c.charCodeAt(0);
-    v *= 17;
-    v = v % 256;
-    return v;
-  });
-  return v;
-});
-console.log("Part 1: ", hashes.sum());
+const steps = readInput(dir, ",");
 
 // part 1
+const getHashes = (steps: string[], stopCh: string[] = []) => {
+  const hashes = steps.map((str) => {
+    let v = 0;
+    for (let c of str) {
+      if (stopCh.includes(c)) break;
+      v += c.charCodeAt(0);
+      v = (v * 17) % 256;
+    }
+    return v;
+  });
+  return hashes;
+};
+
+const hashes1 = getHashes(steps);
+console.log("Part 1: ", hashes1.sum());
+
+// part 2
+const hashes2 = getHashes(steps, ["-", "="]);
+const boxes: [string, string][][] = new Array(256).fill(null);
+
+steps.forEach((s, i) => {
+  const match = s.match(/(.*)(\=|\-)(.*)/);
+  const hI = hashes2[i];
+
+  if (!match) return;
+  const [, label, op, v] = match;
+
+  // store box
+  if (op === "=") {
+    boxes[hI] ??= [];
+    const bI = boxes[hI].findIndex((b) => b[0] === label);
+
+    if (bI !== -1) boxes[hI][bI][1] = v; // update lens value
+    else boxes[hI].push([label, v]); // otherwise add new lens
+
+    // remove lens by label
+  } else if (op === "-") {
+    if (boxes[hI]) boxes[hI] = boxes[hI].filter((b) => b[0] !== label);
+  }
+});
+
+const p2 = boxes.reduce(
+  (acc, b, bI) =>
+    acc + (b || []).reduce((a, s, sI) => a + (bI + 1) * (sI + 1) * +s[1], 0),
+  0
+);
+
+console.log("Part 2: ", p2);
