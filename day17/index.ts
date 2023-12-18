97c56ea97bd6232d4671986641f993af98e9e40b
Fadi George <fadii925@gmail.com>
2023-12-17 16:37:26 -0800

    day 17 p1

--- day17/index.ts
+++ day17/index.ts
@@ -1,126 +1,80 @@
-import { MinHeap } from "../MinHeap";
-import { AStar, type Position, readInput, printGrid } from "../helpers";
+import { PriorityQueue } from "datastructures-js";
+import { readInput } from "../helpers";
 
 const dir = import.meta.dir;
 const lines = readInput(dir, "\n");
 
 const heatGrid: number[][] = [];
-lines.forEach((line, rI) => {
-  heatGrid.push(line.split("").map(Number));
+lines.forEach((line) => {
+  heatGrid.push(line.split("").map((v) => +v));
 });
 
-enum Dir {
-  Up,
-  Down,
-  Right,
-  Left,
-}
+const goal = { r: heatGrid.length - 1, c: heatGrid[0].length - 1 };
 
-type PQ = {
-  hl: number; // heat loss
-  r: number; // row
-  c: number; // col
-  dr: number; // delta row
-  dc: number; // delta col
-  dirCount: number; // number of times direction has been traveled
-  value: number;
-  paths: [number, number][]; // debug
+type QueueItem = {
+  r: number;
+  c: number;
+  dr: number;
+  dc: number;
+  hl: number;
+  dirCount: number;
 };
 
-const end = { r: heatGrid.length - 1, c: heatGrid[0].length - 1 };
-const heap = new MinHeap<PQ>();
+const q = new PriorityQueue<QueueItem>((p1, p2) => p1.hl - p2.hl);
+q.push({
+  r: 0,
+  c: 0,
+  dr: 0,
+  dc: 0,
+  hl: 0,
+  dirCount: 0,
+});
+
 const seen = new Set<string>();
+const dirs = [
+  [-1, 0], // up
+  [1, 0], // down
+  [0, -1], // left
+  [0, 1], // right
+];
 
-printGrid(heatGrid);
-console.log("end", end);
+let p1 = 0;
+while (q.size() > 0) {
+  const { hl, r, c, dr, dc, dirCount } = q.pop();
 
-heap.add({
-  hl: heatGrid[0][0],
-  r: 0,
-  c: 0,
-  dr: 0,
-  dc: 0,
-  dirCount: 0,
-  value: heatGrid[0][0],
-  paths: [[0, 0]],
-});
-heap.add({
-  hl: heatGrid[0][0],
-  r: 0,
-  c: 0,
-  dr: 0,
-  dc: 0,
-  dirCount: 0,
-  value: heatGrid[0][0],
-  paths: [[0, 0]],
-});
-
-while (heap.length > 0) {
-  const { hl, r, c, dr, dc, dirCount, value, paths } = heap.remove()!;
-  // console.log({ hl, r, c, dr, dc, dirCount, value, paths });
-
-  if (r === end.r && c === end.c) {
-    console.log("Part 1: ", { hl, paths });
-    console.log(
-      "what",
-      paths.reduce((acc, [r, c]) => acc + heatGrid[r][c], 0)
-    );
-    paths.forEach(([r, c]) => {
-      heatGrid[r][c] = "#";
-    });
-    printGrid(heatGrid);
+  // stop at bottom right
+  if (r === goal.r && c === goal.c) {
+    p1 = hl;
     break;
   }
 
-  const key = `${r}-${c}-${dr}-${dc}-${dirCount}`;
+  if (seen.has(`${r}-${c}-${dr}-${dc}-${dirCount}`)) continue;
+  seen.add(`${r}-${c}-${dr}-${dc}-${dirCount}`);
 
-  if (seen.has(key)) continue;
-  seen.add(key);
+  let i = 0;
+  for (const [dr2, dc2] of dirs) {
+    i++;
+    if (dr2 === -dr && dc2 == -dc) continue;
 
-  if (dirCount < 3 && !(dr === 0 && dc === 0)) {
-    const nr = r + dr;
-    const nc = c + dc;
-    const v2 = heatGrid[nr]?.[nc];
-    if (v2 !== undefined) {
-      // console.log("1");
-      heap.add({
-        hl: hl + value,
-        r: nr,
-        c: nc,
-        dr,
-        dc,
-        dirCount: dirCount + 1,
-        paths: [...paths, [nr, nc]],
-        value: v2,
-      });
-    }
-  }
-
-  [
-    [-1, 0], // top
-    [1, 0], // bottom
-    [0, 1], // right
-    [0, -1], // left
-  ].forEach(([dr2, dc2]) => {
     const nr = r + dr2;
     const nc = c + dc2;
+    const newDirCount = dr === dr2 && dc === dc2 ? dirCount + 1 : 1;
 
-    const v2 = heatGrid[nr]?.[nc];
-    if (
-      v2 !== undefined &&
-      !(dr === dr2 && dc === dc2) &&
-      !(dr === -dr2 && dc === -dc2)
-    ) {
-      heap.add({
-        hl: value + v2,
+    if (heatGrid[nr]?.[nc]) {
+      const v = heatGrid[nr][nc];
+
+      // prevent going 3 steps in the same direction
+      if (newDirCount > 3) continue;
+
+      q.enqueue({
         r: nr,
         c: nc,
         dr: dr2,
         dc: dc2,
-        dirCount: 1,
-        value: v2,
-        paths: [...paths, [nr, nc]],
+        hl: hl + v,
+        dirCount: newDirCount,
       });
     }
-  });
+  }
 }
+console.log("Part 1: ", p1);
