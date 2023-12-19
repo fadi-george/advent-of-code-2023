e35891cd5884643b45f5a831f9c056a0d7a0cb5c
Fadi George <fadii925@gmail.com>
2023-12-19 01:17:45 -0800

    day 19 p2

--- day19/index.ts
+++ day19/index.ts
@@ -2,15 +2,19 @@ import { readInput } from "../helpers";
 
 const dir = import.meta.dir;
 const workflows: Record<string, Function[]> = {};
+const workflowRanges: Record<string, [string, string, number, string][]> = {};
 
 const [w, ratings] = readInput(dir, "\n\n");
 
 w.split("\n").forEach((line) => {
   let [, name, r] = line.match(/(.*)\{(.*)\}/)!;
   workflows[name] = [];
+  workflowRanges[name] = [];
+
   const rArr = r.split(",");
   rArr.slice(0, rArr.length - 1).forEach((c) => {
     const [_, cat, op, val, ret] = c.match(/([xmas])(\>|\<)(\d+)\:(.*)/)!;
+
     workflows[name].push((x: number, m: number, a: number, s: number) => {
       let _var = { x: x, m: m, a: a, s: s }[cat]!;
       let _val = +val;
@@ -23,9 +27,12 @@ w.split("\n").forEach((line) => {
       }
       return false;
     });
+
+    workflowRanges[name].push([cat, op, +val, ret]);
   });
 
   const lastC = rArr[rArr.length - 1];
+  workflowRanges[name].push([lastC]);
   workflows[name].push(() => {
     if (lastC === "R") return "reject";
     if (lastC === "A") return "accept";
@@ -63,4 +70,123 @@ const checkPart = (p: [number, number, number, number]) => {
 };
 
 const p1 = params.filter(checkPart).reduce((a, p) => a + p.sum(), 0);
-console.log(p1);
+console.log("Part 1: ", p1);
+
+// const cMap = {
+//   x: 0,
+//   m: 1,
+//   a: 2,
+//   s: 3,
+// };
+
+// let minRanges = [4000, 4000, 4000, 4000];
+// // const workflow = workflowRanges["in"];
+
+// const getMinRange = (wID: string, ranges: number[]) => {
+//   const workflow = workflowRanges[wID];
+//   const elseC = workflow[workflow.length - 1][0];
+//   let hasACase = false;
+
+//   for (let i = 0; i < workflow.length - 1; i++) {
+//     const [cat, op, val, ret] = workflow[i];
+//     let ci = cMap[cat as keyof typeof cMap]; // get index of category
+
+//     // else case
+//     if (cat === "A") {
+//       // ???
+//       break;
+//     }
+
+//     // min range doesn't change
+//     if (ret === "A" && elseC === "A") {
+//       continue;
+//     }
+
+//     if (ret === "A") {
+//       // min range changes
+//       if (op === ">") ranges[ci] = 4000 - val;
+//       else if (op === "<") ranges[ci] = val - 1;
+//       continue;
+//     }
+
+//     if (ret === "R") continue;
+
+//     getMinRange(ret, ranges);
+//   }
+
+//   // // check opposite conditions of preceding rules
+//   // if (elseC === "A" && !hasACase) {
+//   //   for (let i = 0; i < workflow.length - 1; i++) {
+//   //     const [cat, op, val] = workflow[i];
+//   //     let ci = cMap[cat as keyof typeof cMap]; // get index of category
+
+//   //     if (op === ">") ranges[ci] = val;
+//   //     else if (op === "<") ranges[ci] = 4000 - val + 1;
+//   //   }
+//   // }
+// };
+
+// getMinRange("in", [4000, 4000, 4000, 4000], ["x", "m", "a", "s"]);
+
+// const getMinRange = (wID: string, ranges: number[]) => {
+//   const workflow = workflowRanges[wID];
+//   const elseC = workflow[workflow.length - 1][0];
+//   let hasACase = false;
+
+//   for (let i = 0; i < workflow.length - 1; i++) {
+//     const [cat, op, val, ret] = workflow[i];
+//     let ci = cMap[cat as keyof typeof cMap]; // get index of category
+
+//     // min range doesn't change
+//     if (ret === "A" && elseC === "A") {
+//       hasACase = true;
+//       continue;
+//     }
+
+//     if (ret === "A") {
+//       // min range changes
+//       if (op === ">") ranges[ci] = 4000 - val;
+//       else if (op === "<") ranges[ci] = val - 1;
+//       continue;
+//     }
+
+//     if (ret === "R") continue;
+
+//     getMinRange(ret, ranges);
+//   }
+
+//   // check opposite conditions of preceding rules
+//   if (elseC === "A" && !hasACase) {
+//     for (let i = 0; i < workflow.length - 1; i++) {
+//       const [cat, op, val] = workflow[i];
+//       let ci = cMap[cat as keyof typeof cMap]; // get index of category
+
+//       if (op === ">") ranges[ci] = val;
+//       else if (op === "<") ranges[ci] = 4000 - val + 1;
+//     }
+//   }
+// };
+
+// const minRanges = Object.keys(workflowRanges).map((w) => {
+//   let cMins = [4000, 4000, 4000, 4000];
+
+//   getMinRange(w, cMins);
+
+//   return cMins;
+// });
+// console.log(minRanges);
+
+// for (let i = 0; i < workflow.length - 1; i++) {
+//   const [cat, op, val, ret] = workflow[i];
+//   let ci = cMap[cat as keyof typeof cMap]; // get index of category
+
+//   // min range doesn't change
+//   if (ret === "A") continue;
+
+//   if (ret === "R") {
+//     // min range changes
+//     if (op === ">") miNRanges[ci] = 4000 - val;
+//     else if (op === "<") miNRanges[ci] = val - 1;
+//     continue;
+//   }
+// }
